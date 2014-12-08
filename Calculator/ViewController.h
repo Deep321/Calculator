e08f4d4031497491c50fed8853bdf82755c50967
Deep Prasad <Deep@Deeps-MacBook-Air.local>
2014-12-07 23:03:44 -0500

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Deep Prasad on 2014-12-07.
+//  Copyright (c) 2014 Deep Prasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Deep Prasad on 2014-12-07.
+//  Copyright (c) 2014 Deep Prasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
